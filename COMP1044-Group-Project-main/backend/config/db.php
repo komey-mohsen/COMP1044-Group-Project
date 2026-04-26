8cd7ff006addce382489ffb1d43d563cece015d6
Ali al komey <137861119+komey-mohsen@users.noreply.github.com>
2026-04-26 18:21:12 +0800

    fixed some files to properly run

--- COMP1044-Group-Project-main/backend/config/db.php
+++ COMP1044-Group-Project-main/backend/config/db.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect("localhost", "root", "root", "internship_system");
+$conn = mysqli_connect("localhost", "root", "", "internship_system");
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
--- COMP1044-Group-Project-main/backend/results/view_results.php
+++ COMP1044-Group-Project-main/backend/results/view_results.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli("localhost", "root", "root", "internship_system");
+$conn = new mysqli("localhost", "root", "", "internship_system");
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
